cad932b9587038d572990f63c00a8759853c87a9
María Belén Arévalo Esquivel <belensoez@gmail.com>
2023-08-23 22:22:24 -0500

    Tarea actualizada

---
+++ Parcial1/Tarea1/javascript.js
@@ -0,0 +1,99 @@
+
+
+function XML() {
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+
+    xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+    });
+
+    xhr.open("GET", "https://apimocha.com/belenapi/twitch");
+    xhr.setRequestHeader("Accept", "*/*");
+
+    xhr.send(data);
+}
+
+async function FetchAsync() {
+    let headersList = {
+        "Accept": "*/*",
+       }
+       
+       let response = await fetch("https://apimocha.com/belenapi/twitch", { 
+         method: "GET",
+         headers: headersList
+       });
+       
+       let data = await response.text();
+       console.log(data);  
+}
+
+function FetchPromesa() {
+    let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+       };
+       
+       fetch("https://apimocha.com/belenapi/twitch", { 
+         method: "GET",
+         headers: headersList
+       })
+         .then(response => {
+           if (!response.ok) {
+             throw new Error(`HTTP error! Status: ${response.status}`);
+           }
+           return response.text();
+         })
+         .then(data => {
+           console.log(data);
+         })
+         .catch(error => {
+           console.error('Error:', error);
+         });       
+}
+
+$(document).ready(function(){
+    $('#btnJQuery').click(function(){
+        realizarPeticionJQuery();
+    });
+});
+
+function realizarPeticionJQuery() {
+    const url = "https://apimocha.com/belenapi/twitch";
+
+    $.get(url, function(response) {
+        console.log(response);
+    })
+    .done(function() {
+        console.log("Petición completada exitosamente.");
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Error en la petición:", textStatus, errorThrown);
+    });
+}
+
+async function AxiosRequest() {
+    let headersList = {
+        "Accept": "*/*"
+    }
+
+    let reqOptions = {
+        url: "https://apimocha.com/belenapi/twitch",
+        method: "GET",
+        headers: headersList,
+    }
+
+    try {
+        let response = await axios.request(reqOptions);
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
+
