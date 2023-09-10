9dc6d373ad646e04bab908680d8045844786ebd2
María Belén Arévalo Esquivel <belensoez@gmail.com>
2023-09-10 18:30:07 -0500

    Ajustes a ServerBD

--- Parcial2/ServidorBD/index.js
+++ Parcial2/ServidorBD/index.js
@@ -1,50 +1,62 @@
 const express = require('express');
 const morgan = require('morgan');
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const mssql = require('mssql');
+const { ConnectionPool } = require('mssql');
 const app = express();
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-app.use(morgan('combined', { stream: accessLogStream }));
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}))
 
-// Configuración de la conexión a SQL Server
 const config = {
-    user: '...',       
-    password: '...',  
-    server: 'localhost',
-    database: '...',   
+    user: 'barevalo',
+    password: 'are_p@ss23*',
+    server: 'affinitysql.database.windows.net', 
+    database: 'Affinity', 
     options: {
-        encrypt: true, // Para conexiones seguras
-    },
-}
+        encrypt: true
+    }
+};
 
-// Ruta para obtener usuarios
 app.get("/usuarios", async (req, res) => {
     try {
-        // Conectar a SQL Server
-        const pool = await mssql.connect(config);
 
-        // Realizar una consulta
-        const result = await pool.request().query('SELECT * FROM Lab5 WHERE id = @value', {
-            name: 'value', 
-            type: mssql.Int,
-            value: 123, 
-        });
+        const pool = new ConnectionPool(config);
+        await pool.connect();
 
-        // Enviar resultados como JSON
+        const result = await pool.request().query('SELECT * FROM PruebaApiRest');
+
+        await pool.close();
+
         res.json(result.recordset);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error al realizar la consulta a la base de datos.' });
-    } finally {
-        // Cerrar la conexión a SQL Server
-        mssql.close();
+    } catch (error) {
+        console.error("Error de conexión:", error.message);
+        res.status(500).json({ mensaje: "Error de conexión" });
     }
 });
 
-// Iniciar el servidor
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Servidor Express en ejecución en el puerto ${port}`);
+app.get("/usuarios/:edad", async (req, res) => {
+    console.log(req.params.edad);
+    const { edad } = req.params;
+    const pool = new ConnectionPool(config);
+    await pool.connect();
+
+    try {
+        const result = await pool.request().query('SELECT * FROM PruebaApiRest WHERE edad=' + edad);
+
+        if (result.recordset.length === 0) {
+            res.status(404).json({ mensaje: "Usuario no existe" });
+        } else {
+            res.json(result.recordset);
+        }
+    } catch (error) {
+        console.error("Error de consulta:", error.message);
+        res.status(500).json({ mensaje: "Error de consulta" });
+    } finally {
+        await pool.close();
+    }
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("Server Express escuchando en el puerto " + PORT);
 });
